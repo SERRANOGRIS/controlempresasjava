df0bdbb153e11985d1653a9256414a4ebf0c689d
Gris <serranoescalante1994@gmail.com>
2023-08-08 16:21:09 -0600

    Clases

---  ControlEmpresas.EntidadesDeNegocio/src/controlempresas/entidadesdenegocio/Contacto.java
+++  ControlEmpresas.EntidadesDeNegocio/src/controlempresas/entidadesdenegocio/Contacto.java
@@ -1,13 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlempresas.entidadesdenegocio;
+import java.util.ArrayList;
 
-/**
- *
- * @author ss220
- */
+
 public class Contacto {
+    private int id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private String celular;
+    private int top_aaux;
+    private ArrayList<Empresa>empresa;
+
+    public Contacto() {
+    }
+
+    public Contacto(int id, String nombre, String email, String telefono, String celular, int top_aaux, ArrayList<Empresa> empresa) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.celular = celular;
+        this.top_aaux = top_aaux;
+        this.empresa = empresa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public int getTop_aaux() {
+        return top_aaux;
+    }
+
+    public void setTop_aaux(int top_aaux) {
+        this.top_aaux = top_aaux;
+    }
+
+    public ArrayList<Empresa> getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(ArrayList<Empresa> empresa) {
+        this.empresa = empresa;
+    }
+
+   
     
 }
---  ControlEmpresas.EntidadesDeNegocio/src/controlempresas/entidadesdenegocio/Usuario.java
+++  ControlEmpresas.EntidadesDeNegocio/src/controlempresas/entidadesdenegocio/Usuario.java
@@ -1,13 +1,126 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controlempresas.entidadesdenegocio;
 
-/**
- *
- * @author ss220
- */
+import java.time.LocalDate;
+
 public class Usuario {
+     private int id;
+    private int IdRol;
+    private String nombre;
+    private String apellido;
+    private String login;
+    private  String password;
+    private byte estatus;
+    private LocalDate fechaRegistro;
+    private int top_aux;
+    private String confirmarPassword_aux;
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, int IdRol, String nombre, String apellido, String login, String password, byte estatus, LocalDate fechaRegistro, int top_aux, String confirmarPassword_aux, Rol rol) {
+        this.id = id;
+        this.IdRol = IdRol;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.login = login;
+        this.password = password;
+        this.estatus = estatus;
+        this.fechaRegistro = fechaRegistro;
+        this.top_aux = top_aux;
+        this.confirmarPassword_aux = confirmarPassword_aux;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdRol() {
+        return IdRol;
+    }
+
+    public void setIdRol(int IdRol) {
+        this.IdRol = IdRol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(byte estatus) {
+        this.estatus = estatus;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+
+    public String getConfirmarPassword_aux() {
+        return confirmarPassword_aux;
+    }
+
+    public void setConfirmarPassword_aux(String confirmarPassword_aux) {
+        this.confirmarPassword_aux = confirmarPassword_aux;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
     
+    
+
 }
